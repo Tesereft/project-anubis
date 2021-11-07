1213a7a0b5912e09b556ce0866239c509537eadf
dc143c <jhony.wt@gmail.com>
2021-11-07 01:04:51 -0300

    GitHook update!

--- src/components/partials/SearchBar.js
+++ src/components/partials/SearchBar.js
@@ -1,19 +1,15 @@
 import MaterialIcon from "@material/react-material-icon";
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from "react-bootstrap/FormControl";
 import "./SearchBar.css";
 
 export default function SearchBar(){
     return (
         <div className="topbar">
-            <div className="searchbar-container">
-                <div class="search-box">
-                    <input id="searchbar" autocomplete="none" type="text" className="searchbar-input" placeholder="Search..."/>
-                    <div className="search-button">
-                        <MaterialIcon icon="search" className="search-icon"/>
-                    </div>
-                </div>
-                <div className="result-box">
-                </div>
-            </div>
+            <InputGroup className="mb-6">
+                <FormControl aria-label="Dollar amount (with dot and two decimal places)" />
+                <InputGroup.Text><MaterialIcon icon="search" className="search-icon"/></InputGroup.Text>
+            </InputGroup>
         </div>
     );
 }
